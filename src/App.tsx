90e5d8c218d9e463c4eb1d4d817004ccd87f7d20
Derii Eugene <eugenederii@gmail.com>
2024-11-02 19:48:27 +0200

    feat: create app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold text-red-500">Hello world! s</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
