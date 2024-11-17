21ec6afe03ade3eca8b76cc3d3bf50edfe6facfe
Derii Eugene <eugenederii@gmail.com>
2024-11-17 02:23:22 +0200

    fix: fix

--- src/layouts/pageLayuot/pageLayout.tsx
+++ src/layouts/pageLayuot/pageLayout.tsx
@@ -6,7 +6,7 @@ import { type PageLayoutProps } from "./pageLayout.interface";
 
 export const PageLayout: FC<PageLayoutProps> = ({ children, currentStep }) => {
   return (
-    <div className="flex flex-col md:flex-row w-full h-full max-w-[1100px] md:bg-white bg-mongo rounded-[20px] md:shadow-lg md:p-6 mx-auto md:mt-28">
+    <div className="flex flex-col md:flex-row w-full h-full max-w-[1100px] md:bg-white bg-mongo rounded-[20px] md:shadow-lg md:p-6 mx-auto md:mt-[100px]">
       <StepIndicator currentStep={currentStep} />
       <div className="md:bg-white flex flex-col relative bg-transparent md:static -top-[88px] md:top-0 gap-4 px-[20px] md:px-14 w-full rounded-lg ">
         {children}
